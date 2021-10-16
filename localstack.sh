bc27393fac535bfa5d35ce083713910ce8222e07
Brian Edwards <brian.mabry.edwards@gmail.com>
2021-10-16 18:29:11 -0500

    Services

--- localstack.sh
+++ localstack.sh
@@ -1,6 +1,20 @@
-#!/bin/bash
+#!/bin/zsh
 
 set -eux
 
-LAMBDA_EXECUTOR=local localstack start --host
+# https://localstack.cloud/features
+# https://docs.aws.amazon.com/cli/latest/reference/#available-services
+SERVICES="iam"
+SERVICES="${SERVICES},lambda"
+SERVICES="${SERVICES},apigateway"
+SERVICES="${SERVICES},s3"
+SERVICES="${SERVICES},cloudformation"
+SERVICES="${SERVICES},cloudwatch"
+SERVICES="${SERVICES},ecr"
+SERVICES="${SERVICES},events"
+SERVICES="${SERVICES},secretsmanager"
+SERVICES="${SERVICES},stepfunctions"
+SERVICES="${SERVICES},sts"
+
+SERVICES="${SERVICES}" LAMBDA_EXECUTOR=local localstack start --host
 pgrep kinesis | xargs kill -9
